1e48daa537b945c3f36a6a21f953303b8059e766
Tymur Kubai <phddir@gmail.com>
2024-11-22 16:11:19 +0100

    Initial commit: init playwright, add README template add gitignore add github actions

---
+++ README.md
@@ -0,0 +1,56 @@
+# ThreatFabric-test-assignment
+
+## Introduction
+
+TODO
+
+The Playwright framework has been selected to provide test coverage for this application. More information about Playwright can be found [here](https://playwright.dev/docs/intro).
+
+## Requirements
+
+- `NodeJS 20+` - can be downloaded from [here](https://nodejs.org/en/download/package-manager)
+
+## Setup Project
+
+1. Clone the repository:
+
+   ```sh
+   git clone git@github.com:sirdir/abn-amro-test-assignment.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```sh
+   cd abn-amro-test-assignment
+   ```
+
+3. Install project dependencies:
+
+   ```sh
+   npm install
+   ```
+
+4. Install playwright required dependencies (will ask for `sudo`):
+
+   ```sh
+   npx playwright install --with-deps chromium
+   ```
+
+
+## TODO mayber smth else will be added during development
+
+## Usage
+
+TODO
+
+## Reporting
+
+TODO
+
+## CI
+
+TODO
+
+## Static fixes Analysis
+
+TODO
---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./tests",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [["html", { open: "always" }], ["list"]],
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    // baseURL: 'http://127.0.0.1:3000',
+    screenshot: { mode: "only-on-failure", fullPage: true },
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: { mode: "retain-on-failure", sources: true },
+  },
+
+  /* Configure projects for Chrome only */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
